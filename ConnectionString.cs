e9653d5e4e8b0dad7912f6c4269dce247f0922db
Tim Osse <timosse448@gmail.com>
2017-06-18 15:33:09 -0700

    ADO.NET cumulative project
    Logic, Syntax, Methods, Classes, Interfaces, LINQ and Collections, LINQ to SQL

---
+++ ConnectionString.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// Holds connection string information this application uses to connect to databases
+/// </summary>
+public class ConnectionString
+{
+    public static string ATMConnectionString = "Data Source=(localdb)\\Projects;Initial Catalog=ATM;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False";
+
+}
+
